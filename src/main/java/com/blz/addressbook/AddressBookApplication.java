a6498ab87d515e4596e4fa22b21b3a6eca7b6e55
ameyabhasme <ameyabhasme5@gmail.com>
2021-10-19 00:39:13 +0530

    Added: Lombok dependency and replaced getters setters and used logger SLF4J

--- src/main/java/com/blz/addressbook/AddressBookApplication.java
+++ src/main/java/com/blz/addressbook/AddressBookApplication.java
@@ -2,13 +2,17 @@ package com.blz.addressbook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import lombok.extern.slf4j.Slf4j;
 
 @SpringBootApplication
+@Slf4j
 public class AddressBookApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AddressBookApplication.class, args);
-		System.out.println("Running Successfully");
+		ApplicationContext context = SpringApplication.run(AddressBookApplication.class, args);
+		log.info("Running Successfully!! In {} Environment", context.getEnvironment().getProperty("environment"));
 	}
 
 }
--- src/main/java/com/blz/addressbook/model/Contact.java
+++ src/main/java/com/blz/addressbook/model/Contact.java
@@ -2,36 +2,20 @@ package com.blz.addressbook.model;
 
 import com.blz.addressbook.dto.ContactDTO;
 
-public class Contact {
+import lombok.Data;
+
+public @Data class Contact {
 
 	private int id;
 	private String firstName;
 	private String lastName;
 	
+	public Contact() {}
 	
 	public Contact(int id, ContactDTO contactDTO) {
 		this.id = id;
 		this.firstName = contactDTO.firstName;
 		this.lastName = contactDTO.lastName;
-	}
-	
-	public int getId() {
-		return id;
-	}
-	public String getFirstName() {
-		return firstName;
-	}
-	public String getLastName() {
-		return lastName;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
 	}
 	
 }
